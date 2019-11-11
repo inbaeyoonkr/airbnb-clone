ca04226c5c7fc24dbe1ddb81aef58eea9ad9448b
ringo2455 <iby2455@gmail.com>
2019-11-11 21:53:16 +0900

    #7 까지 함

---
+++ rooms/admin.py
@@ -0,0 +1,86 @@
+from django.contrib import admin
+from . import models
+
+
+@admin.register(models.RoomType, models.Facility, models.Amenity, models.HouseRule)
+class ItemAdmin(admin.ModelAdmin):
+
+    """ Item Admin Definition """
+
+    list_display = ("name", "used_by")
+
+    def used_by(self, obj):
+        return obj.rooms.count()
+
+
+@admin.register(models.Room)
+class RoomAdmin(admin.ModelAdmin):
+
+    """ Room Admin Definition """
+
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "city", "price")},
+        ),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")},),
+        ("Spaces", {"fields": ("guests", "bedrooms", "beds", "baths",)}),
+        (
+            "More About The Space",
+            {
+                "classes": ("collapse",),
+                "fields": ("amenities", "facilities", "house_rules"),
+            },
+        ),
+        ("Last Detail", {"fields": ("host",)}),
+    )
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_amenities",
+        "count_photos",
+    )
+
+    ordering = ("name", "price", "bedrooms")
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "city",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "country",
+    )
+    search_fields = ("=city", "host__username")
+
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_rules",
+    )
+
+    def count_amenities(self, obj):
+        return obj.amenities.count()
+
+    def count_photos(self, obj):
+        return obj.photos.count()
+
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+
+    """ Photo Admin Definition """
+
+    pass
