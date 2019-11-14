dc93a7ceb1a30003d72f8ec2de4f2bd1e514f750
ringo2455 <iby2455@gmail.com>
2019-11-14 23:30:17 +0900

    CBV로 DetailView 만들기

--- rooms/views.py
+++ rooms/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render, redirect
 from django.http import Http404
 from django.urls import reverse
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from . import models
 
 
 class HomeView(ListView):
 
-    """ Home View Definition"""
+    """ HomeView Definition"""
 
     model = models.Room
     paginate_by = 10
@@ -16,9 +16,7 @@ class HomeView(ListView):
     context_object_name = "rooms"
 
 
-def room_detail(request, pk):
-    try:
-        room = models.Room.objects.get(pk=pk)
-        return render(request, "rooms/detail.html", context={"room": room})
-    except models.Room.DoesNotExist:
-        raise Http404()
+class RoomDetailView(DetailView):
+    """ RoomDetailView Definition """
+
+    model = models.Room
