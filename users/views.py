424030ec5ef43f0f6c73741d9993bbd97ee777ed
ringo2455 <iby2455@gmail.com>
2019-11-26 00:27:57 +0900

    Finishing Github Login

--- users/views.py
+++ users/views.py
@@ -1,3 +1,5 @@
+import os
+import requests
 from django.views.generic import FormView
 from django.shortcuts import redirect, reverse
 from django.urls import reverse_lazy
@@ -62,3 +64,73 @@ def complete_verification(request, key):
         # to do: add error messag
         pass
     return redirect(reverse("core:home"))
+
+
+def github_login(request):
+    """ Github Login View Definition """
+
+    client_id = os.environ.get("GH_ID")
+    redirect_uri = "http://127.0.0.1:8000/users/login/github/callback"
+    return redirect(
+        f"https://github.com/login/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&scope=read:user"
+    )
+
+
+class GithubException(Exception):
+    pass
+
+
+def github_callback(request):
+    """ Github Callback View Definition """
+    try:
+        code = request.GET.get("code", None)
+        client_id = os.environ.get("GH_ID")
+        client_secret = os.environ.get("GH_SECRET")
+        if code is not None:
+            token_request = requests.post(
+                f"https://github.com/login/oauth/access_token?client_id={client_id}&client_secret={client_secret}&code={code}",
+                headers={"Accept": "application/json"},
+            )
+            token_json = token_request.json()
+            error = token_json.get("error", None)
+            if error is not None:
+                raise GithubException()
+            else:
+                access_token = token_json.get("access_token")
+                profile_request = requests.get(
+                    "https://api.github.com/user",
+                    headers={
+                        "Authorization": f"token {access_token}",
+                        "Accept": "application/json",
+                    },
+                )
+                profile = profile_request.json()
+                print(profile)
+                username = profile.get("login", None)
+                if username is not None:
+                    name = profile.get("name")
+                    email = profile.get("email")
+                    bio = profile.get("bio")
+                    try:
+                        user = models.User.objects.get(email=email)
+                        if user.login_method != models.User.LOGIN_GITHUB:
+                            raise GithubException()
+                    except models.User.DoesNotExist:
+                        user = models.User.objects.create(
+                            username=email,
+                            email=email,
+                            bio=bio,
+                            first_name=name,
+                            login_method=models.User.LOGIN_GITHUB,
+                        )
+                        user.set_unusable_password()
+                        user.save()
+                    login(request, user)
+                    return redirect(reverse("core:home"))
+                else:
+                    raise GithubException()
+        else:
+            raise GithubException()
+    except GithubException:
+        # send error message
+        return redirect(reverse("users:login"))
