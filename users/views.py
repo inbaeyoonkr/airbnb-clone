d1aa6e614570e1967e1db324afd632cdf68d4af2
ringo2455 <iby2455@gmail.com>
2019-12-11 21:02:49 +0900

    set success_url through the next_arg

--- users/views.py
+++ users/views.py
@@ -16,7 +16,6 @@ class LoginView(mixins.LoggedOutOnlyView, FormView):
 
     template_name = "users/login.html"
     form_class = forms.LoginForm
-    success_url = reverse_lazy("core:home")
 
     def form_valid(self, form):
         email = form.cleaned_data.get("email")
@@ -26,6 +25,13 @@ class LoginView(mixins.LoggedOutOnlyView, FormView):
             messages.success(self.request, f"Welcome back {user.first_name}")
             login(self.request, user)
         return super().form_valid(form)
+
+    def get_success_url(self):
+        next_arg = self.request.GET.get("next")
+        if next_arg is not None:
+            return next_arg
+        else:
+            return reverse("core:home")
 
 
 def log_out(request):
