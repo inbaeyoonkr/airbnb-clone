bc314ec0a35631fce07ee149b726e69a7d189413
ringo2455 <iby2455@gmail.com>
2019-12-24 16:59:43 +0900

    see favs only login status

--- lists/views.py
+++ lists/views.py
@@ -8,6 +8,8 @@ from . import models
 def toggle_room(request, room_pk):
     action = request.GET.get("action", None)
     room = room_models.Room.objects.get_or_none(pk=room_pk)
+    print(request.user)
+
     if room is not None and action is not None:
         the_list, _ = models.List.objects.get_or_create(
             user=request.user, name="My Favorite Houses"
