fbabb5ea1ebfeb0c57b5a06f1c9a6bde5ee8159e
ringo2455 <iby2455@gmail.com>
2019-12-29 19:27:51 +0900

    Static Files

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,7 @@ DJANGO_APPS = [
     "django.contrib.staticfiles",
 ]
 
-THIRD_PARTY_APPS = ["django_countries", "django_seed"]
+THIRD_PARTY_APPS = ["django_countries", "django_seed", "storages"]
 
 PROJECT_APPS = [
     "core.apps.CoreConfig",
@@ -160,9 +160,20 @@ EMAIL_FROM = "Ian@sandbox11d492805361432d8ee7ce8eef46788b.mailgun.org"
 # Locale
 LOCALE_PATHS = (os.path.join(BASE_DIR, "locale"),)
 
-# Sentry
 
 if not DEBUG:
+    # S3
+    DEFAULT_FILE_STORAGE = "config.custom_storages.UploadStorage"
+    STATICFILES_STORAGE = "config.custom_storages.StaticStorage"
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME = "airbnb-clone-ianyoon"
+    AWS_AUTO_CREATE_BUCKET = True
+    AWS_DEFAULT_ACL = "public-read"
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
+
+    # Sentry
     sentry_sdk.init(
         dsn=os.environ.get("SENTRY_URL"),
         integrations=[DjangoIntegration()],
