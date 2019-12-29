421920462f08b869bb3c51ea52183e32321e4c9c
ringo2455 <iby2455@gmail.com>
2019-12-29 16:17:35 +0900

    setting DB

--- config/settings.py
+++ config/settings.py
@@ -100,10 +100,10 @@ else:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
-            "HOST": os.environ.get("HOST"),
-            "NAME": os.environ.get("NAME"),
-            "USER": os.environ.get("USER"),
-            "PASSWORD": os.environ.get("PASSWORD"),
+            "HOST": os.environ.get("RDS_HOST"),
+            "NAME": os.environ.get("RDS_NAME"),
+            "USER": os.environ.get("RDS_USER"),
+            "PASSWORD": os.environ.get("RDS_PASSWORD"),
             "PORT": "5432",
         }
     }
